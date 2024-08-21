eb99b12ea1e87af0678dae10858e687025f19f03
Arnaud <hayon.arnaud@gmail.com>
2024-08-21 10:30:10 +0200

    version web terminee mais sans le background. A suivre : adaptation version mobile en responsive

---
+++ script.js
@@ -0,0 +1,34 @@
+const button = document.querySelector('.submit');
+const input = document.querySelector('.email');
+const formatEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const iconError = document.querySelector('.icon-error');
+const textError = document.querySelector('.text-error');
+let message = [];
+
+button.addEventListener('click', e => {
+    message = [];
+
+    e.preventDefault();
+
+    const email = input.value;
+
+
+    if (email == '' || email == undefined) {
+        message.push('Veuillez rentrer votre email');
+        iconError.style.display = 'block';
+        textError.innerText = message[0];
+
+    } else if (!email.match(formatEmail)) {
+        message.push('Veuillez rentrer un email valide');
+        iconError.style.display = 'block';
+        textError.innerText = message[0];
+
+    }
+
+    if (!message.length > 0) {
+
+        textError.innerText = 'Merci de vous etre inscrit.e a notre newsletter'
+
+    }
+});
+
